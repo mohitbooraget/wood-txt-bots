70b7ecf103a2f456d491ebda8d3b0f524f45526b
PiroLeader <158609590+PiroLeader@users.noreply.github.com>
2024-05-18 17:21:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,6 +1,6 @@
 # SudoR2spr WOODcraft
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "18429621"
-api_hash = "2034b81303744d1dd2c7ffc02e21cfe2"
-bot_token = "6774837267:AAGmdNGetOhN5svx6N9iUQzKwFLIWnI_Sb4"
+api_id = "14914136"
+api_hash = "e41475e32c4cedf6b5a2b493a32308c3"
+bot_token = "7027721633:AAF84A3jeH2SCspEgLm8rFaBVk4NavahsrI"
